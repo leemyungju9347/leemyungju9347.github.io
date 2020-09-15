a9f34d509c3adfdefe30babb2cb359df4b0b07a5
leemyungju9347 <myungju9347@naver.com>
2020-09-15 23:41:51 +0900

    폴더 이미지 변경, section 구조 및 css 수정

--- js/main.js
+++ js/main.js
@@ -2,25 +2,52 @@ const gnbList = document.querySelectorAll('header > nav > .gnb-menu > li');
 const sectionElms = document.querySelectorAll('main > .section-wrap > section');
 const sectionWrap = document.querySelector('main > .section-wrap')
 const body = document.querySelector('body');
+// const typing = document.querySelector('section.about .typing');
 
-console.log(gnbList);
-
+// console.log(typing);
 for(let i = 0; i < gnbList.length; i++){
     gnbList[i].addEventListener('click', (ev)=>{
         ev.preventDefault();
 
         // 전체 active 지우기
-        for(let i = 0; i < sectionElms.length; i++){
-            sectionElms[i].classList.remove('active');
+        for(let j = 0; j < sectionElms.length; j++){
+            sectionElms[j].classList.remove('active');
+            gnbList[j].classList.remove('on');
             body.classList.remove('black');
-            sectionWrap.classList.remove('change');
         }
 
         sectionElms[i].classList.add('active');
-        sectionWrap.classList.add('change');
+        gnbList[i].classList.add('on');
 
         if( sectionElms[i].classList.contains('about') ) {
             body.classList.add('black');
+            // typingEffect();
+            // setInterval(typingEffect,1000)
         }
     })
+
+    
+}
+
+
+function typingEffect(){
+    let text = '제 스킬을 소개합니다 🙋‍♀️';
+    let i = 0;
+
+    // setInterval(()=>{
+    //     console.log('a');
+    // })
+
+    // if( i < text.length ){
+    //     document.querySelector('section.about .typing').innerHTML += text[i];
+    //     console.log(document.querySelector('section.about .typing').innerHTML);
+    //     i++;
+
+    //     // console.log(text[i]);
+        
+    // }
+
+    
+
+    
 }
